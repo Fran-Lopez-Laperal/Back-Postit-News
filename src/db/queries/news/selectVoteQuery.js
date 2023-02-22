8a534a4e807659b7e85b5643f684d5921461d925
ErReberde <joseluis.gomez.cano@hotmail.es>
2023-02-22 19:23:11 +0100

    Fate: Creado sistema de votos. Si se vota alguna news en la que ya exista voto de usuario, se quita su voto.

---
+++ src/db/queries/news/selectVoteQuery.js
@@ -0,0 +1,24 @@
+const { generateError } = require("../../../helpers")
+const getDB = require("../../getDB")
+
+
+const selectVoteQuery = async (idUser, idNew)=>{
+    let connection
+    
+    try{
+        connection = await getDB()
+
+        let vote = await connection.query(`
+            SELECT * FROM votes WHERE idUser= ? AND idNew= ?
+        `, [idUser, idNew])
+
+        if (vote.length > 0) return vote[0]
+        
+        return false
+
+    }finally{
+        connection?.release()
+    }
+}
+
+module.exports = selectVoteQuery
