7d765cd932134f4537e25452259ae28e51db751a
ines <4cines@gmail.com>
2023-02-24 19:02:46 +0100

    cambios categorias

--- src/db/queries/news/insertCategoryQuery.js
+++ src/db/queries/news/insertCategoryQuery.js
@@ -1,9 +1,21 @@
+const { generateError } = require("../../../helpers");
 const getDB = require("../../getDB");
 
 const insertCategoryQuery = async (category) => {
   let connection;
   try {
     connection = await getDB();
+
+    //comprobar si ya existe ese nombre de categoría
+    const [categories] = await connection.query(`SELECT name FROM categories`);
+
+    for (let data of categories) {
+      const { name } = data;
+      console.log(name);
+      if (name === category) {
+        generateError("La categoría ya existe");
+      }
+    }
 
     await connection.query(`INSERT INTO categories (name) VALUES (?)`, [
       category,
