f239a91e970e4ceb59d6da40fd3ca418038e4710
ErReberde <joseluis.gomez.cano@hotmail.es>
2023-02-27 14:01:03 +0100

    prueba selectnews

--- src/db/queries/news/selectNewsQuery.js
+++ src/db/queries/news/selectNewsQuery.js
@@ -10,8 +10,8 @@ const selectNewsQuery = async()=>{
         let [news] = await connection.query(`
         SELECT N.*,V.id, V.idUser, V.idNew, V.value, count(N.id) as numValues FROM news N 
         LEFT JOIN votes V ON N.id = V.idNew 
-        GROUP BY idNew 
-        ORDER BY numValues DESC;
+        GROUP BY idNew
+        ORDER BY numValues DESC
         `)
 
         return news
