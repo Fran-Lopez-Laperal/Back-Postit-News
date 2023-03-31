32e01ca0640f5626e4747a408d8c7abd192313d1
Meira81 <guadalupe.alonso.meira@gmail.com>
2023-03-31 14:17:47 +0200

    arreglo token en loginUser

--- src/controllers/users/loginUser.js
+++ src/controllers/users/loginUser.js
@@ -1,45 +1,39 @@
-const selectUserByEmailQuery = require("../../db/queries/users/selectUserByEmailQuery")
-const { generateError } = require("../../helpers")
-const jwt = require('jsonwebtoken');
-const bcrypt = require('bcrypt')
+const selectUserByEmailQuery = require("../../db/queries/users/selectUserByEmailQuery");
+const { generateError } = require("../../helpers");
+const jwt = require("jsonwebtoken");
+const bcrypt = require("bcrypt");
 
 const loginUser = async (req, res, next) => {
+  try {
+    const { email, password } = req.body;
 
-    try {
-        const { email, password } = req.body
-
-        if (!email || !password) {
-            generateError('Faltan campo de usuario', 400)
-        }
-
-        const user = await selectUserByEmailQuery(email)
-
-        const validPassword = await bcrypt.compare(password, user.password)
-
-        if (!validPassword) {
-            generateError('Contraseña incorrecta', 401);
-        }
-
-        const userInfo = {
-            id: user.id
-        }
-
-        const token = jwt.sign(userInfo, process.env.SECRET, {
-            expiresIn: '7d'
-        })
-
-        res.send({
-            status: 'ok',
-            data: {
-                token,
-            }
-        })
-
-    } catch (error) {
-        next(error)
+    if (!email || !password) {
+      generateError("Faltan campo de usuario", 400);
     }
 
-}
+    const user = await selectUserByEmailQuery(email);
 
+    const validPassword = await bcrypt.compare(password, user.password);
 
-module.exports = loginUser
+    if (!validPassword) {
+      generateError("Contraseña incorrecta", 401);
+    }
+
+    const userInfo = {
+      id: user.id,
+    };
+
+    const token = jwt.sign(userInfo, process.env.SECRET, {
+      expiresIn: "7d",
+    });
+
+    res.send({
+      status: "ok",
+      data: token,
+    });
+  } catch (error) {
+    next(error);
+  }
+};
+
+module.exports = loginUser;
