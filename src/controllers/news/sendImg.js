6083a6a0bd7da0bdf255c9715a1c2f3d98a834ec
Meira81 <guadalupe.alonso.meira@gmail.com>
2023-04-04 10:57:30 +0200

    rutafotos

--- src/controllers/news/sendImg.js
+++ src/controllers/news/sendImg.js
@@ -1,8 +1,19 @@
-const sendImg = (req, res) => {
+/* const sendImg = (req, res) => {
   const { filename } = req.params;
   res.sendFile(
     `C:\\Users\\corbu\\Desktop\\Desarrollo\\Back-Postit-News\\src\\uploads\\${filename}`
   );
 };
 
+module.exports = sendImg; */
+
+const path = require("path");
+
+const sendImg = (req, res) => {
+  const { filename } = req.params;
+  const imgPath = path.join(__dirname, "..", "..", "uploads", filename);
+  res.sendFile(imgPath);
+};
+
 module.exports = sendImg;
+
