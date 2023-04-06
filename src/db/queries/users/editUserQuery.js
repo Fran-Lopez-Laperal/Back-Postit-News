413e901abe87128294e69d4173012495836b864f
ines <4cines@gmail.com>
2023-04-06 20:08:31 +0200

    cambios update user

--- src/db/queries/users/editUserQuery.js
+++ src/db/queries/users/editUserQuery.js
@@ -3,34 +3,34 @@ const getDB = require("../../getDB");
 
 const editUserQuery = async (name, email, bio, idUser) => {
   let connection;
-  console.log(idUser);
+  console.log("name", name);
   try {
     connection = await getDB();
     if (name) {
       const [users] = await connection.query(
-        `SELECT id FROM users WHERE name = ?`,
+        `SELECT id, name FROM users WHERE name = ?`,
         [name]
       );
-      if (users.length > 0) {
+
+      if (users.length > 0 && users[0].name !== name) {
         generateError("El nombre de usuario no está disponible", 403);
       }
 
-      
-      const prueba = await connection.query(`UPDATE users SET name  = ? WHERE id = ?`, [
-        name,
-        idUser,
-      ]);
+      const prueba = await connection.query(
+        `UPDATE users SET name  = ? WHERE id = ?`,
+        [name, idUser]
+      );
       console.log(prueba);
     }
-  
 
     if (email) {
       const [users] = await connection.query(
-        `SELECT id FROM users WHERE email = ?`,
+        `SELECT id, email FROM users WHERE email = ?`,
         [email]
       );
-      if (users.length > 0) {
-        generateError(`El email ${users.email} ya esta asociado a un usuario`);
+
+      if (users.length > 0 && users[0].email !== email) {
+        generateError(`El email ya esta asociado a un usuario`);
       }
 
       await connection.query(`UPDATE users SET  email = ? WHERE id = ?`, [
@@ -40,8 +40,6 @@ const editUserQuery = async (name, email, bio, idUser) => {
     }
 
     if (bio) {
-      await connection.query(`SELECT id FROM users WHERE bio = ?`, [bio]);
-
       await connection.query(`UPDATE users SET bio = ? WHERE id = ? `, [
         bio,
         idUser,
