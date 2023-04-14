e27a7ee18d9be7725b06e1aa4f143fc7ab60aa12
Fran <fransanxenxo@gmail.com>
2023-04-14 15:20:25 +0200

    valor votos

--- src/db/queries/news/selectNewByIdQuery.js
+++ src/db/queries/news/selectNewByIdQuery.js
@@ -8,14 +8,15 @@ const selectNewByIdQuery = async (idNew) => {
 
     let [infoNew] = await connection.query(
        `
-       SELECT C.name as nameCategory, U.name, U.email, U.avatar, N.*, 
-    SUM(CASE WHEN V.value = "like" THEN 1 ELSE 0 END) as totalLikes,
-    SUM(CASE WHEN V.value = "dislike" THEN 1 ELSE 0 END) as totalDisLikes
-    FROM news N 
-    LEFT JOIN votes V ON N.id = V.idNew
-    LEFT JOIN users U ON U.id = N.idUser
-    LEFT JOIN categories C ON C.id = N.idCategory
-    GROUP BY N.id HAVING N.id=? `,
+      SELECT C.name as nameCategory, U.name, U.email, U.avatar, N.*, 
+      SUM(CASE WHEN V.value = "like" THEN 1 ELSE 0 END) as totalLikes,
+      SUM(CASE WHEN V.value = "dislike" THEN 1 ELSE 0 END) as totalDisLikes,
+      V.value as userVote
+      FROM news N 
+      LEFT JOIN votes V ON N.id = V.idNew
+      LEFT JOIN users U ON U.id = N.idUser
+      LEFT JOIN categories C ON C.id = N.idCategory
+      GROUP BY N.id HAVING N.id=? `,
       [idNew]
     );
 
