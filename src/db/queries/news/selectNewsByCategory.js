2fd15a042507e81fd7c9e5dace6127b40b81f09a
Fran <fransanxenxo@gmail.com>
2023-04-17 11:08:56 +0200

    añadido nombre usuario y nombre categoria en selectNewsByCategory

--- src/db/queries/news/selectNewsByCategory.js
+++ src/db/queries/news/selectNewsByCategory.js
@@ -15,7 +15,7 @@ const selectNewsByCategory = async (idCategory) => {
     console.log("nameCategory", nameCategory);
 
     const [newsByCategory] = await connection.query(
-      ` SELECT count(V.idNew) as numVotes,V.value="like", V.value="dislike", N.id as idNew, N.*, U.avatar FROM news N 
+      ` SELECT count(V.idNew) as numVotes,V.value="like", V.value="dislike", N.id as idNew, N.*, U.avatar, U.name as userName, C.name as nameCategory  FROM news N 
         LEFT JOIN votes V ON N.id = V.idNew 
         INNER JOIN categories C ON N.idCategory = C.id
         INNER JOIN users U ON U.id = N.idUser
