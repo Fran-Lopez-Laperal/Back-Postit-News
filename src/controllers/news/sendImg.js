d6f2577b920ddae16c012d3447c13cc88e2ce6aa
ines <4cines@gmail.com>
2023-04-24 21:11:43 +0200

    añadido .env-example y photoNull.jpg

--- src/controllers/news/sendImg.js
+++ src/controllers/news/sendImg.js
@@ -1,23 +1,29 @@
-const { generateError } = require('../../helpers');
-const sharp = require('sharp');
-const path = require('path');
+const { generateError } = require("../../helpers");
+const sharp = require("sharp");
+const path = require("path");
 
 const sendImg = async (req, res, next) => {
   try {
     const { filename } = req.params;
-    const imagePath = path.join(__dirname, '..', '..', 'uploads', filename ?? 'foto.jpg');
+
+    let imagePath;
+
+    if (filename === "null") {
+      imagePath = path.join(__dirname, "..", "..", "..", "photoNull.jpg");
+    } else {
+      imagePath = path.join(__dirname, "..", "..", "..", "uploads", filename);
+    }
 
     const resizedImage = await sharp(imagePath).resize(800).toBuffer();
 
     const fileExtension = path.extname(imagePath).substring(1);
     const contentType = `image/${fileExtension}`;
 
-    res.set('Content-Type', contentType);
+    res.set("Content-Type", contentType);
     res.send(resizedImage);
   } catch (error) {
-    next(generateError('Error al enviar la imagen', 500));
+    next(generateError("Error al enviar la imagen", 500));
   }
 };
 
 module.exports = sendImg;
-
