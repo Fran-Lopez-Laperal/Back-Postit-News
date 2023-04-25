bdaea246dbf31da0547bc9a413690a9e4ec0113e
ines <4cines@gmail.com>
2023-04-25 13:23:13 +0200

    funciona sendImg después de la liada

--- src/controllers/news/sendImg.js
+++ src/controllers/news/sendImg.js
@@ -5,16 +5,23 @@ const path = require("path");
 const sendImg = async (req, res, next) => {
   try {
     const { filename } = req.params;
-    const imagePath = path.join(
-      __dirname,
-      "..",
-      "..",
-      "uploads",
-      filename ?? "foto.jpg"
-    );
+    console.log("filename", filename);
+
+    let imagePath;
+
+    if (filename !== "null") {
+      imagePath = path.join(__dirname, "..", "..", "uploads", filename).trim();
+    } else {
+      imagePath = path
+        .join(__dirname, "..", "..", "uploads", "foto.jpg")
+        .trim();
+    }
+
     const resizedImage = await sharp(imagePath).resize(800).toBuffer();
+
     const fileExtension = path.extname(imagePath).substring(1);
     const contentType = `image/${fileExtension}`;
+
     res.set("Content-Type", contentType);
     res.send(resizedImage);
   } catch (error) {
