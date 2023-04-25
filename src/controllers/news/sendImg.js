1c5f80be860fee3a7bd6818a058ce64c03105d5d
ines <4cines@gmail.com>
2023-04-25 12:25:11 +0200

    sendImg deshechos los cambios de ayer

--- src/controllers/news/sendImg.js
+++ src/controllers/news/sendImg.js
@@ -5,20 +5,16 @@ const path = require("path");
 const sendImg = async (req, res, next) => {
   try {
     const { filename } = req.params;
-
-    let imagePath;
-
-    if (filename === "null") {
-      imagePath = path.join(__dirname, "..", "..", "..", "photoNull.jpg");
-    } else {
-      imagePath = path.join(__dirname, "..", "..", "..", "uploads", filename);
-    }
-
+    const imagePath = path.join(
+      __dirname,
+      "..",
+      "..",
+      "uploads",
+      filename ?? "foto.jpg"
+    );
     const resizedImage = await sharp(imagePath).resize(800).toBuffer();
-
     const fileExtension = path.extname(imagePath).substring(1);
     const contentType = `image/${fileExtension}`;
-
     res.set("Content-Type", contentType);
     res.send(resizedImage);
   } catch (error) {
